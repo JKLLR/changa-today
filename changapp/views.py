a413bc17a1c6ed60154998babf01e7f25c6cfbdb
JKLLR <jeffhuria@gmail.com>
2022-06-21 11:08:49 +0300

    add views for registrations

--- changapp/views.py
+++ changapp/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def signin(request):
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You have successfuly loged in")
+            return redirect ('/')
+    return render(request,'registration/login.html')
+
+def register(request):
+    if request.method == "POST":
+        username=request.POST["username"]
+        first_name=request.POST["first_name"]
+        last_name=request.POST["last_name"]
+        email=request.POST["email"]
+        password1=request.POST["password1"]
+        password2=request.POST["password2"]
+
+        if password1 !=password2:
+            messages.error(request,'Password do not match')
+            return render('/register')
+        new_user=User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password1,
+        ) 
+        new_user.save() 
+        return render (request,'registration/login.html')
+    return render(request,'registration/register.html')
 
 
