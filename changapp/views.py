1395b56dfc6d277978303996e5c73b92729739d5
JKLLR <jeffhuria@gmail.com>
2022-06-21 11:25:06 +0300

    add login template

--- changapp/views.py
+++ changapp/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='/login/')
 def home(request):
     return render(request, 'home.html')
 
