f5ebcb2b03c144156cda4aec01d8cc1e48e5c458
JKLLR <jeffhuria@gmail.com>
2023-01-04 17:10:07 +0300

    deploy to railway

--- changapp/tests.py
+++ changapp/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
-
-# Create your tests here.
+def to_jaden_case(string):
+    textArr = string.split("my name is jeff")
+    jadenCaseArr = []
+    for word in textArr:
+        jadenCaseArr.append(word.capitalize())
+        
+    return "my name is jeff".join(jadenCaseArr)
